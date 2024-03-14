50039bd296e4e4ead478742ff4840e585126eb61
Kstyk <80002380+Kstyk@users.noreply.github.com>
2024-03-14 19:21:11 +0100

    Add register component, set some routes, configuring tailwind and daisy

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth.component';
 import { RouterModule } from '@angular/router';
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, RegisterComponent],
   providers: [],
   imports: [RouterModule, AuthRoutingModule],
 })
