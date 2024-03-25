bfe008ae276ea70e80ffeaee425890f9a3ecf888
Seweryn Drag <dragseweryn@zohomail.eu>
2024-03-25 23:36:46 +0100

    added multiple file upload method

--- backend/Services/FileService.cs
+++ backend/Services/FileService.cs
@@ -7,6 +7,7 @@ namespace backend.Services
     public interface IFileService
     {
         Task<string> UploadFileAsync(IFormFile file);
+        Task<string[]> UploadFilesAsync(IFormFile[] files);
     }
 
     public class FileService : IFileService
@@ -17,6 +18,54 @@ namespace backend.Services
         {
             _dbContext = dbContext;
             _userContextService = userContextService;
+        }
+
+        public async Task<string[]> UploadFilesAsync(IFormFile[] files)
+        {
+            // 1. Check if files are not empty
+            if (files == null || files.Length == 0)
+            {
+                throw new Exception("Files are empty");
+            }
+
+            // 2. Check if user is authenticated
+            var userId = _userContextService.GetUserId;
+
+            if (userId == null)
+            {
+                throw new UnauthorizedAccessException("User is not authenticated");
+            }
+
+            var filePaths = new List<string>();
+
+            foreach (var file in files)
+            {
+                // 3. Create a new file entity
+                var fileEntity = new backend.Entities.File
+                {
+                    Name = file.FileName,
+                    OwnerId = userId.Value,
+                    UploadedAt = DateTime.UtcNow
+                };
+
+                // 4. Generate unique file name
+                var filePath = Guid.NewGuid().ToString() + "_" + file.FileName;
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "Files", filePath);
+
+                // 5. Save the file to disk
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                // 6. Save the file path to the database
+                fileEntity.Path = path;
+                _dbContext.Files.Add(fileEntity);
+                await _dbContext.SaveChangesAsync();
+                filePaths.Add(filePath);
+            }
+
+            return filePaths.ToArray();
         }
 
         public async Task<string> UploadFileAsync(IFormFile file)
